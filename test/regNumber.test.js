dfa193825efd53fe2c186ead3ee8a79b9e41edb9
banelengwane <banelengwane@gmail.com>
2018-10-28 20:12:38 +0200

    test running

--- test/regNumber.test.js
+++ test/regNumber.test.js
@@ -4,7 +4,7 @@ const pg = require('pg');
 const Pool = pg.Pool;
 
 // we are using a special test database for the tests
-const connectionString = process.env.DATABASE_URL || 'postgresql://coder:coder123@localhost/registrations';
+const connectionString = process.env.DATABASE_URL || 'postgresql://coder:coder123@localhost/regTests';
 
 const pool = new Pool({
     connectionString
@@ -12,23 +12,19 @@ const pool = new Pool({
 
 describe('The basic database web app', function () {
     beforeEach(async function () {
-        // clean the tables before each test run
-        // await pool.query('delete from towns;');
         await pool.query('delete from regsTb;');
     });
 
     it('should return all regs in db', async function () {
         let registration = Registrator(pool);
         await registration.regNumbers('CA 123-123');
-        await registration.regNumbers('CY 223-223');
-        await registration.regNumbers('CJ 992-992');
-
-        // console.log(await reg.InputReg('CA 123-123'));
+        await registration.regNumbers('CY 45695');
+        await registration.regNumbers('CJ 123');
 
         assert.deepStrictEqual(await registration.allRegs(),
-            [ { regNumber: 'CA 123-123' },
-                { regNumber: 'CY 223-223' },
-                { regNumber: 'CJ 992-992' }
+            [ { 'regnumber': 'CA 123-123' },
+                { 'regnumber': 'CY 45695' },
+                { 'regnumber': 'CJ 123' }
             ]);
     });
     after(function () {
