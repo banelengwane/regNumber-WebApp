f64844435226f275f7df2fe7f88b22db4791c47b
banelengwane <banelengwane@gmail.com>
2018-11-15 12:42:11 +0200

    final commit

--- registration_number.js
+++ registration_number.js
@@ -1,22 +1,26 @@
 module.exports = function Registrator (pool) {
     async function regNumbers (reg) {
-        reg = reg.toUpperCase();
-        let longFormat = /^[A-Z]{2,3}\s[0-9]{3}(\-)[0-9]{3}$/;
-        let shortFormat = /^[A-Z]{2,3}\s[0-9]{6}$/;
-        if (longFormat.test(reg) || shortFormat.test(reg)) {
-            let startString = reg.split(' ', 1).join();
-            let currentTown = await pool.query('select id from towns where startStr = $1', [startString]);
+        if (typeof reg === 'string') {
+            reg = reg.toUpperCase();
+            let longFormat = /^[A-Z]{2,3}\s[0-9]{3}(\-)[0-9]{3}$/;
+            let shortFormat = /^[A-Z]{2,3}\s[0-9]{6}$/;
+            if (longFormat.test(reg) || shortFormat.test(reg)) {
+                let startString = reg.split(' ', 1).join();
+                let currentTown = await pool.query('select id from towns where startStr = $1', [startString]);
 
-            if (currentTown.rows.length === 1) {
-                let resultReg = await pool.query('select * from regsTb where regNumber = $1', [reg]);
+                if (currentTown.rows.length === 1) {
+                    let resultReg = await pool.query('select * from regsTb where regNumber = $1', [reg]);
 
-                if (resultReg.rows.length === 0) {
-                    await pool.query('insert into regsTb(town_id, regNumber) values ($1, $2)', [currentTown.rows[0].id, reg]);
-                } else if (resultReg.rowCount > 0) {
-                    return 'This registration has been added before';
+                    if (resultReg.rows.length === 0) {
+                        await pool.query('insert into regsTb(town_id, regNumber) values ($1, $2)', [currentTown.rows[0].id, reg]);
+                    } else if (resultReg.rowCount > 0) {
+                        return 'This registration has been added before';
+                    }
+                } else {
+                    return 'Enter a Registration number from: CA, CAW, CY and/or CJ';
                 }
             } else {
-                return 'Enter a Registration number from: CA, CAW, CY and/or CJ';
+                return 'Enter a Registration as required: CA 123456/CA 123-456';
             }
         } else {
             return 'Enter a Registration as required: CA 123456/CA 123-456';
--- test/regNumber.test.js
+++ test/regNumber.test.js
@@ -17,14 +17,14 @@ describe('The basic database web app', function () {
 
     it('should return all regs in db', async function () {
         let registration = Registrator(pool);
-        await registration.regNumbers('CA 1');
-        await registration.regNumbers('CY 45695');
-        await registration.regNumbers('CAW 123');
+        await registration.regNumbers('CA 111-300');
+        await registration.regNumbers('CY 456959');
+        await registration.regNumbers('CAW 123-982');
 
         assert.deepStrictEqual(await registration.allRegs(),
-            [  { 'regnumber': 'CA 1' },
-                { 'regnumber': 'CY 45695' },
-                { 'regnumber': 'CAW 123' }
+            [ { 'regnumber': 'CA 111-300' },
+                { 'regnumber': 'CY 456959' },
+                { 'regnumber': 'CAW 123-982' }
             ]);
     });
 
@@ -32,38 +32,38 @@ describe('The basic database web app', function () {
         let registration = Registrator(pool);
         await registration.regNumbers('CA 123-123');
         await registration.regNumbers('CY 45695');
-        await registration.regNumbers('CA 123');
+        await registration.regNumbers('CA 123123');
         await registration.regNumbers('CAW 123');
 
         assert.deepEqual(await registration.whichTown('CA'),
-            [ { 'regnumber': 'CA 123-123' }, { 'regnumber': 'CA 123' } ]);
+            [ { 'regnumber': 'CA 123-123' }, { 'regnumber': 'CA 123123' } ]);
     });
 
     it('should filter and return bellville registrations', async function () {
         let registration = Registrator(pool);
         await registration.regNumbers('CA 123-123');
-        await registration.regNumbers('CY 45695');
-        await registration.regNumbers('CA 123');
+        await registration.regNumbers('CY 456958');
+        await registration.regNumbers('CA 555-696');
 
         assert.deepEqual(await registration.whichTown('CY'),
-            [ { 'regnumber': 'CY 45695' } ]);
+            [ { 'regnumber': 'CY 456958' } ]);
     });
 
     it('should filter and return paarl registrations', async function () {
         let registration = Registrator(pool);
         await registration.regNumbers('CA 123-123');
         await registration.regNumbers('CY 45695');
-        await registration.regNumbers('CJ 123');
+        await registration.regNumbers('CJ 985-659');
 
         assert.deepEqual(await registration.whichTown('CJ'),
-            [ { 'regnumber': 'CJ 123' } ]);
+            [ { 'regnumber': 'CJ 985-659' } ]);
     });
 
     it('should filter and return george registrations', async function () {
         let registration = Registrator(pool);
         await registration.regNumbers('CAW 123-123');
-        await registration.regNumbers('CA 45695');
-        await registration.regNumbers('CA 123');
+        await registration.regNumbers('CA 456958');
+        await registration.regNumbers('CA 714-652');
 
         assert.deepEqual(await registration.whichTown('CAW'),
             [ { 'regnumber': 'CAW 123-123' } ]);
@@ -71,8 +71,8 @@ describe('The basic database web app', function () {
 
     it('should return messages for registrations added more than once', async function () {
         let registration = Registrator(pool);
-        await registration.regNumbers('CA 123')
-        assert.deepEqual(await registration.regNumbers('CA 123'),
+        await registration.regNumbers('CA 123-123')
+        assert.deepEqual(await registration.regNumbers('CA 123-123'),
             'This registration has been added before');
     });
 
@@ -80,14 +80,14 @@ describe('The basic database web app', function () {
         let registration = Registrator(pool);
 
         assert.deepEqual(await registration.regNumbers('ND 7898'),
-            'Enter a Registration number from: CA, CAW, CY and/or CJ');
+            'Enter a Registration as required: CA 123456/CA 123-456');
     });
 
     it('should return messages for non-strings', async function () {
         let registration = Registrator(pool);
 
         assert.deepEqual(await registration.regNumbers(123),
-            'Please enter a valid Registration Number');
+            'Enter a Registration as required: CA 123456/CA 123-456');
     });
     after(function () {
         pool.end();
